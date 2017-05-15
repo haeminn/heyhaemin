1df241e725bfddee446cc2d2cc0d5651735383bc
Haemin Park <haeminp@umich.edu>
2017-05-15 14:14:14 -0400

    bubbles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 angular.module('heyhaemin', [])
 .controller('mainController', ['$scope','$location', function($scope, $location) {
-	$scope.me = false;
-	$scope.portfolio = false;
+	// $scope.me = false;
+	// $scope.portfolio = false;
+	$scope.intro = false;
 
 }]);
