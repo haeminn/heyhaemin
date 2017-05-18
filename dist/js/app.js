5fbbbf20600a9527dc575c3d30731fd15c80fe2f
Haemin Park <haeminp@umich.edu>
2017-05-18 15:42:11 -0400

    ng-cloak

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,24 @@
-angular.module('heyhaemin', [])
-.controller('mainController', ['$scope','$location', function($scope, $location) {
-	// $scope.me = false;
-	// $scope.portfolio = false;
+angular.module('heyhaemin',['ui.router'])
+.controller('mainController', [
+	'$scope',
+	'$location',
+	'$state',
+	'$window',
+	function(
+	$scope,
+	$location,
+	$state,
+	$window){
+
 	$scope.intro = false;
 	$scope.doggoClick = false;
 	$scope.namu_heart = false;
+
+	$scope.funziesPageRoute = function()  {
+	  
+	  $state.go('funzies');
+	  console.log("route to a new page");
+	  //$window.location.reload();
+	};
 
 }]);
