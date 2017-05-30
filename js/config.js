fab798379229cc6f8f79a9c2481af3138adc9af9
Haemin Park <haeminp@umich.edu>
2017-05-30 15:09:09 -0400

    router is fixed

--- js/config.js
+++ js/config.js
@@ -1,23 +1,20 @@
 angular.module('heyhaemin')
-.config(['$stateProvider', 
+.config(['$stateProvider',  
 	'$urlRouterProvider', 
 	'$locationProvider', 
-	function (
-		$stateProvider, 
-		$urlRouterProvider, 
+	function($stateProvider,
+		$urlRouterProvider,
 		$locationProvider) {
-  
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  });
 
- $stateProvider
-    .state('funzies', {
-      url:'/funzies.html',
-      controller: 'mainController'
-    });
+		$locationProvider.html5Mode({
+		    enabled: true,
+		    requireBase: false
+		});
 
-  //f$urlRouterProvider.otherwise('/');  
-
+		$stateProvider
+		.state('funzies', {
+		    url:'/funzies.html',
+		    controller: 'mainController'
+		});
+  		$urlRouterProvider.otherwise('/');
 }]);
